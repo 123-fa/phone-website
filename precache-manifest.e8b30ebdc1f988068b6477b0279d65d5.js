75a5b41d9087002b05ba09ee32022cf827390d84
123-fa <orafufrancis22@gmail.com>
2023-06-19 10:16:42 -0700

    Updates

---
+++ precache-manifest.e8b30ebdc1f988068b6477b0279d65d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "19f8f912b527dff9eb6b",
+    "url": "/123-fa/Phone-ecommerce-website.git/static/css/main.e3dff6fd.chunk.css"
+  },
+  {
+    "revision": "19f8f912b527dff9eb6b",
+    "url": "/123-fa/Phone-ecommerce-website.git/static/js/main.54196c12.chunk.js"
+  },
+  {
+    "revision": "6d558ada3893883aa614",
+    "url": "/123-fa/Phone-ecommerce-website.git/static/js/runtime~main.62557810.js"
+  },
+  {
+    "revision": "299671807c8844ee4cc4",
+    "url": "/123-fa/Phone-ecommerce-website.git/static/css/2.d9a2f428.chunk.css"
+  },
+  {
+    "revision": "299671807c8844ee4cc4",
+    "url": "/123-fa/Phone-ecommerce-website.git/static/js/2.b53d4e52.chunk.js"
+  },
+  {
+    "revision": "85b67d58420062fbecec",
+    "url": "/123-fa/Phone-ecommerce-website.git/static/js/3.eff08ea3.chunk.js"
+  },
+  {
+    "revision": "05b99227e68d27504b7c135655c15a25",
+    "url": "/123-fa/Phone-ecommerce-website.git/static/media/telephone.05b99227.jpg"
+  },
+  {
+    "revision": "c4b681c8b02faa4023bd4add27868fb8",
+    "url": "/123-fa/Phone-ecommerce-website.git/index.html"
+  }
+];
